4f1904b374e85882be2cd487b789b3e8cabf4927
Stefan <dewenter.stefan@gmail.com>
2019-09-08 14:51:15 +0200

    fin

--- functions.py
+++ functions.py
@@ -28,7 +28,7 @@ def newpointcore(point, x, windspeed, winddirection, steps):  # erstellen der co
 def newpointpoly(point, x, windspeed, winddirection, degreesplit, target1, target2, steps):  # erstellen der polygone
     newpointlon1 = point[x][0] + (180 / (math.pi * 6137000)) * math.cos(math.radians(winddirection + degreesplit)) \
              / math.cos(point[x][1] * math.pi/180) * (windspeed * steps * 60)  # * math.cos(lenghtcorrection(degreesplit))
-    print(math.cos(math.radians(winddirection+degreesplit)))
+    # print(math.cos(math.radians(winddirection+degreesplit)))
     newpointlat1 = point[x][1] + (180 / (math.pi * 6137000)) * math.sin(math.radians(winddirection + degreesplit)) \
              * (windspeed * steps * 60)  # * math.sin(lenghtcorrection(degreesplit))
     if x >= 1:
@@ -48,16 +48,46 @@ def newpointpoly(point, x, windspeed, winddirection, degreesplit, target1, targe
     target2.append([newpointlon2, newpointlat2])
     target2.append([point[x+1][0], point[x+1][1]])
 
+# mathematische funktionen
 
 def createangle(input, distance):
     return math.degrees(math.asin(input/math.sqrt((input * input) + (distance * distance))))
 
 
-def lenghtcorrection(alpha):
-    return 1/math.sin(math.radians(90 - alpha))
-
-
 def distancepoints(x1, y1, x2, y2):
     return 6137000 * math.acos(math.sin(math.radians(y1)) * math.sin(math.radians(y2)) + math.cos(math.radians(y1))
                     * math.cos(math.radians(y2)) * math.cos(math.radians(x2 - x1)))
+
+
+def creatzoom(windspeed, numbersteps, timesteps):  # generiert dynamische zoomstufen
+    ref = windspeed*numbersteps*timesteps
+    print(ref)
+
+    if ref < 20:
+        zoom = 16
+    elif 20 <= ref < 40:
+        zoom = 15
+    elif 40 <= ref < 60:
+        zoom = 14
+    elif 60 <= ref < 80:
+        zoom = 13
+    elif 80 <= ref:
+        zoom = 12
+
+    return zoom
+
+
+def difusion(mol, temp, pressure, time):  # beschreibt die diffusion quer zum wind
+
+    avogadro = 6.02214076 * 10 ** 23
+    molair = 28
+    boltzmann = 1.380649 * 10 ** -23
+    coldi = 0.3
+    colin = 3
+    distribution = 10
+    first = 0.375 * ((avogadro / math.pi) * ((molair + mol) / (2 * molair * mol))) ** (1 / 2)
+    second = ((boltzmann * temp) ** (2 / 3)) * (pressure * coldi ** 2 * colin)
+    result = math.sqrt(first * second * (time + 1) * 60)
+
+    return result * distribution
 
--- junk/test.py
+++ junk/test.py
@@ -1,15 +1,8 @@
-def addtimestamp(name, time):
+import json
 
-    from PIL import Image, ImageDraw, ImageFont
+with open('data.config') as json_file:
+    data = json.load(json_file)
+    print(data)
+    test = int(data["density"])
+    print(test)
 
-    image = Image.open(str(name) + '.png')
-
-    draw = ImageDraw.Draw(image)
-
-    font = ImageFont.truetype('Arial.ttf', size=45)
-
-    (x, y) = (100, 50)
-    message = str(time)
-    color = 'rgb(255, 0, 0)'
-    draw.text((x, y), message, fill=color, font=font)
-    image.save(str(name) + '.png')
